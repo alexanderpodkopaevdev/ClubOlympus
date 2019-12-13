a0338f139d8b509f857212c81c4c79a96c6a4aa5
Alexander Podkopaev <p-alex1990@mail.ru>
2019-12-13 18:03:07 +0700

    add views on addMemberActivity

--- app/src/main/java/com/example/clubolympus/AddMemberActivity.kt
+++ app/src/main/java/com/example/clubolympus/AddMemberActivity.kt
@@ -2,12 +2,20 @@ package com.example.clubolympus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
+import kotlinx.android.synthetic.main.activity_add_member.*
 
 class AddMemberActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_member)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val genderAdapter = ArrayAdapter.createFromResource(this,R.array.gender_list,android.R.layout.simple_spinner_item)
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spGender.adapter = genderAdapter
     }
 }
